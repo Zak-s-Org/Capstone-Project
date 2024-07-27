d0743c57bd36e8c0482b220777f2090e378258de
Zak Zuniga <zakmanzz95@gmail.com>
2024-07-26 20:25:06 -0500

    Reorganize and bring in back end

---
+++ backend/routes/routes.js
@@ -0,0 +1,17 @@
+const pool = require("../utils/pool")
+const express = require("express")
+const router = express.Router()
+
+router.get("/" , async (req,res) =>{
+console.log("get route executed")
+    try {
+    const result = await pool.query(`
+        SELECT * FROM users;`)
+        res.status(200).json(result.rows)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error: error.message})
+} 
+})
+
+module.exports = router
